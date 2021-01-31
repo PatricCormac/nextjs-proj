f4b1b8245db0311f2f9910eafbb1bae8dbbf00bb
PatricCormac <socolov1897@gmail.com>
2021-01-31 22:07:00 +0500

    first-post page

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Next.js App</title>
+        <title>Next.js Blog</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Next.js App
+      <main>
+        <h1>
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a>first post</a>
+          </Link>
         </h1>
-        <Link href="/about"><a>About</a></Link>
       </main>
-    </div>
+    </>
   )
 }
